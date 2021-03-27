fee77f588a62c1a152a263157a12884e20d0dadf
Gabrielgjs <79715102+Gabrielgjs@users.noreply.github.com>
2021-03-26 23:16:38 -0300

    Add files via upload

---
+++ repetições.js
@@ -0,0 +1,21 @@
+console.log('vai começar')
+for(c=1; c<=4; c++){
+    console.log(c)
+}
+console.log('fim')
+
+/*var c = 1
+do {
+  console.log(`passo ${c}`)
+  c++
+}while(c <= 10)
+*/
+
+
+
+/*var c = 1
+while (c < 6){
+  console.log('tudo bem')
+  c++
+}
+*/
